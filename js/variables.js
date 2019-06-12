e89154a76a45962f0d4da2bc523e277cef662e41
Ivan <kimagure44@yahoo.es>
2019-06-13 01:37:17 +0200

    Eliminación de código obsoleto y que ya no se usa. Separación en módulos

---
+++ js/variables.js
@@ -0,0 +1,17 @@
+const variables = {
+  colorFilms: '#bcaaa4',
+  colorPeople: '#ff8a65',
+  colorPlanets: '#ffb74d',
+  colorSpecies: '#aed581',
+  colorStarships: '#4da0c1',
+  colorVehicles: '#ef9a9a',
+  currentPage: 1,
+  url: "https://swapi.co/api/",
+  allURL: ["films","people","planets","species","starships","vehicles"],
+  apiPrincipal: 0,
+  llamadaApiPrincipal: true,
+  contArrayLlamadaPrincipal: 0,
+  objetoAPI: [],
+};
+
+export default variables;
